960864132b273cf31de5ae9f1a54b42a0b8d2f1a
caesarkrisnaf <caesarkrisna08@gmail.com>
2023-05-13 18:51:13 +0700

    projek uts

---
+++ content/admin/jenis_produk/delete.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../../../koneksi.php';
+$id = $_GET['id'];
+$sql = "DELETE FROM jenis_produk WHERE id= ?";
+$r = $dbh->prepare($sql);
+$r->execute(array($id));
+header('location:../../../index.php?page/admin=jenis_produk');
+
+?>
